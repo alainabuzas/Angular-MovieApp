e8ae8e5937eed32b637095c427ba12c534b2b072
alainabuzas <alaina.buzas@gmail.com>
2017-05-09 14:34:11 -0700

    codealong complete

--- index.js
+++ index.js
@@ -2,5 +2,26 @@
 var movieApp = angular.module('MovieApp', []);
 
 movieApp.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
+    // $scope.searchTerm = '';
+    $scope.movieList = [];
+
+    $scope.search = function() {
+        var req = {
+            url: 'http://www.omdbapi.com',
+            method: 'GET',
+            params: {
+                s: $scope.searchTerm,
+            }
+        }
+
+        $http(req).then(function(response) {
+            console.log(response.data.Search);
+            $scope.movieList = response.data.Search;
+        });
+    }
+
+    $scope.$watch('searchTerm', function(newVal, oldVal) {
+        $scope.search();
+    });
 
 }]);
